19e91325bad81730fc3c8dbce8f513c65ccefc58
jiangxf <157508086@qq.com>
2021-10-29 15:37:57 +0800

    updated

--- gateway/src/main/java/cn/itcast/gateway/GatewayApplication.java
+++ gateway/src/main/java/cn/itcast/gateway/GatewayApplication.java
@@ -2,10 +2,18 @@ package cn.itcast.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    KeyResolver userIdKeyResolver() {//http://localhost:10010/user/1?authorization=admin&userId=abc
+        return exchange -> Mono.just(exchange.getRequest().getQueryParams().getFirst("userId"));
     }
 }
--- order-service/src/main/java/cn/itcast/order/web/OrderController.java
+++ order-service/src/main/java/cn/itcast/order/web/OrderController.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.http.HttpClient;
+//import java.net.http.HttpClient;
 
 @RestController
 @RequestMapping("order")
